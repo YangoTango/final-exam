a48fe36e9466ec4cd54b04d2d32a7bf78321ae1f
YangoTango <rguzman@student.scitech.edu>
2025-11-13 19:03:16 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const registerBtn = document.getElementById('register-btn');
+
+registerBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    alert('You have registered... succesfully');
+});
